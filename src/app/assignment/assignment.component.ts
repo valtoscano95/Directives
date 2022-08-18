3c72104f2eccb1af36fe787dd5eb9587377574e7
Toscano Alcala <kwbt377@mxc02yt3ualvdqdd.azprov.net>
2022-08-18 13:58:32 -0500

    deleted extra code

--- src/app/assignment/assignment.component.ts
+++ src/app/assignment/assignment.component.ts
@@ -26,56 +26,6 @@ export class AssignmentComponent implements OnInit {
       this.buttonClicksClass.push(this.counter);
     }
 
-    console.log('clicks sin class: ' + this.buttonClicks)
-    console.log('clicks con class: ' + this.buttonClicksClass)
-    
-    
-
-
-
-
-
-    //use ngClass and ngStyle
-
-    
-
-
-
-
-
-
-
-    //defines the list and the list element
-    this.ul = document.getElementById('list');
-    this.li = document.createElement("span");
-    // this.li = document.createElement("li");
-
-    // this.li.classList.add('blue');
-    // this.li.innerHTML='hello';
-    // console.log(this.li)
-    
-
-
-    //check if it's the first element, and add a blue background
-    // if(this.buttonClicks.length>4){
-    //   this.li.className='blue';
-    //   console.log(this.li);
-    // }
-
-    //Create a list element using the last element of the array (length-1) and appends it to the ul element
-    // this.li.appendChild(document.createTextNode(this.buttonClicks[this.buttonClicks.length-1]));
-    
-
-    //crear un span
-    this.li.innerHTML="<li [ngClass]=\"{blue: counter>=5}\">test</li>"
-    //cambiar el innerHTML
-    
-
-
-    //check if it's the first element
-    
-    this.ul.appendChild(this.li);
-
   }
 
   constructor() {
